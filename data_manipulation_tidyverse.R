ed6af75c7dc02ab15baf84f561a3f16e0bf85a39
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2022-10-28 12:50:25 +0200

    Basic functions for data manipulation, using the tidyverse packages

---
+++ data_manipulation_tidyverse.R
@@ -0,0 +1,11 @@
+library(dplyr)
+
+# --------------------- OPERATIONS FROM THE TIDYVERSE ----------------------- #
+16 %>% sqrt
+`+`(2, 12)
+16 %>%
+  sqrt %>%
+  log
+
+16 %>% sqrt %>% `+`(18)
+16 |> sqrt() |> `+`(x = _, 18)
